845d69fb475ee9085358591e73cd76ffa2f7d6aa
ciedar <dciesla545@gmail.com>
2023-06-11 13:51:42 +0200

    first

---
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipie } from '../recipies.model';
+@Component({
+  selector: 'app-recipie-list',
+  templateUrl: './recipie-list.component.html',
+  styleUrls: ['./recipie-list.component.css']
+})
+export class RecipieListComponent {
+  name = "sadasd"
+  recipies: Recipie[] = [
+    new Recipie("test2", "test1", "https://img.freepik.com/free-photo/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_2829-19744.jpg?w=1380&t=st=1686430398~exp=1686430998~hmac=0ef6308c7f99450a658fbc149f06ed5920578ef571505796d4cc8e52055b1c58"),
+    new Recipie("sadasd", "sadasda", "sadasdasdsa")
+  ];
+
+}
+
+
+
