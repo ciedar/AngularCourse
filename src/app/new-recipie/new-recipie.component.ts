90cec0a1a97436bb2142089c461adc02cb61207e
ciedar <dciesla545@gmail.com>
2023-07-29 17:00:40 +0200

    pushingProjectOnMyOwn

--- src/app/new-recipie/new-recipie.component.ts
+++ src/app/new-recipie/new-recipie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Recipie } from '../recipies/recipies.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -23,7 +23,8 @@ export class NewRecipieComponent implements OnInit {
     this.newRecipieForm = new FormGroup({
       'recipieName': new FormControl(null, Validators.required),
       'recipieDescription': new FormControl(null, Validators.required),
-      'recipieImagePath': new FormControl(null, Validators.required)
+      'recipieImagePath': new FormControl(null, Validators.required),
+      'ing': new FormArray([])
     })
   }
 
@@ -32,9 +33,28 @@ export class NewRecipieComponent implements OnInit {
     const name = data.value.recipieName
     const desc = data.value.recipieDescription
     const path = data.value.imagePath
-    const ing = [new Ingredient("asdas", 2)]
-    this.newRecipie = new Recipie(name, desc, path, ing)
-    this.recipieService.addNewRecipie(this.newRecipie);
+    const lel = data.value.recipieIngredient
+    console.log(data, lel)
+    // const ing = new Ingredient(data.value.recipieIngredient.recipieName, data.value.recipieIngredient.recipieAmount)
+    // console.log(ing)
+    // console.log(data.value)
+
+    // this.newRecipie = new Recipie(name, desc, path, ing)
+    // this.recipieService.addNewRecipie(this.newRecipie);
+
+  }
+
+
+  onAddHobby() {
+    // const newArrayItem = new FormControl(null, Validators.required);
+    // (<FormArray>this.newRecipieForm.get('recipieIngredient')).push(newArrayItem)
+    // console.log(newArrayItem)
+  }
+
+  getIngredients() {
+    // return (<FormArray>this.newRecipieForm.get('recipieIngredient').controls).
+  }
+  onAddIngredient() {
 
   }
 }
